fcb1748a09346e09d3a286e911dc2f2a0359f70b
SAHIL VERMA <132760049+sahilverma1512@users.noreply.github.com>
2023-10-17 22:04:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".role", {
+    strings: [
+        "Full Stack Developer",
+        "Python Programmer",
+        "Video Editor",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+})
